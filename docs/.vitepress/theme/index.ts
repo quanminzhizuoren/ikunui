5999db276e348c6e286b752394a0cd1db6872f07
dingDC <2096450462@qq.com>
2023-06-27 21:16:47 +0800

    🎨

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,7 @@
 import { EnhanceAppContext } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
-
 import ikunui from '../../../src';
+import './global.less';
 
 export default {
   ...DefaultTheme,
